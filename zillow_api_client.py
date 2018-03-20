9f7e37ad48e3156e244836a8d1adf85cd4adb6cc
algentile <gogentileorgohome@gmail.com>
2018-03-19 21:17:25 -0400

    added citystate zip generation method

--- zillow_api_client.py
+++ zillow_api_client.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from sys import argv
 from urllib.parse import quote_plus
 
@@ -6,6 +7,17 @@ class RestClient(object):
 
 	def __init__(self, zws_id):
 		self.zws_id = zws_id
+
+	def generate_city_state_zip(self, city, state, zip_code):
+		if(city != None and state != None and zip_code != None):
+			encoded_string = quote_plus(",".join([city, " "+state, " "+zip_code]))
+		elif(city != None and state != None and zip_code == None):
+			encoded_string = quote_plus(",".join([city, " "+state]))
+		elif(city != None and state == None and zip_code != None):
+			encoded_string = quote_plus(",".join([city, " "+zip_code]))
+		else:
+			raise TypeError("None of the fields were entered. Need to enter valid fields.")
+		return encoded_string
 
 	def get_zestimate(self, z_pid, rent_zestimate):
 		"""Function that uses zillow api to request 
@@ -20,37 +32,29 @@ class RestClient(object):
 		"""
 		request_url = "http://www.zillow.com/webservice/GetZestimate.htm"
 		parameters = {"zws-id": self.zws_id, "zpid": z_pid, "rentzestimate": rent_zestimate}
-		data = requests.get(request_url, params=parameters).json()
-		return data
+		print(requests.get(request_url, params=parameters))
+		# response = requests.get(request_url, params=parameters).json()
+		# except:
+		# 	print("JSON was invalid: ", response_body)
+		# return response_body
 
 	def get_property_info(self, address, city, state, zip_code, rent_zestimate):
 		"""Finds a property for a specified address
 
 		Args:
 			address (str): The street address of the property you wish to search for.
-			city_state_zip (str): Url encoded string that includes city+state and or zipcode.
+			city (str): The specific city used in search results.
+			state (str): The two letter state identification code used in search results.
+			zip_code (str): The zip_code used in search results.
 			rent_zestimate (bool): Flag to return or not return rest estimate if available.
 
 		Returns:
 			Data the properties located at the specified address.
 		"""
 		request_url = "http://www.zillow.com/webservice/GetSearchResults.htm"
-		parameters = {"zws-id": self.zws_id, "address": address, "citystatezip": zip_code, "rentzestimate": rent_zestimate}
-		data = requests.get(request_url, params=parameters).json()
-		return data
-
-	def generate_city_state_zip(self, city, state, zip_code):
-		if(city != None and state != None and zip_code != None):
-			encoded_string = quote_plus(",".join([city, state, zip_code]))
-		elif(city != None and state != None and zip_code == None):
-			encoded_string = quote_plus(",".join([city, state]))
-		elif(city != None and state == None and zip_code != None):
-			encoded_string = quote_plus(",".join([city, zip_code]))
-		else:
-			raise TypeError("None of the fields were entered. Need to enter valid fields.")
-		return encoded_string
+		parameters = {"zws-id": self.zws_id, "address": quote_plus(address), "citystatezip": self.generate_city_state_zip(city, state, zip_code), "rentzestimate": rent_zestimate}
+		print(parameters)
+		return requests.get(request_url, params=parameters).json()
 
 if __name__ == '__main__':
 	#testing string generation
-	client = RestClient("X1-ZWz1gaiw11wnpn_7c2wf")
-	print(client.generate_city_state_zip("lawrence", "MA", "01841"))
